921cbae0c252ced5efbcb6e677dc514276400240
Saransh Kumar <cryptosingh@gmail.com>
2022-01-09 00:45:54 +0530

    Initial Commit

---
+++ src/main/java/com/saransh/gmoments/GmomentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.saransh.gmoments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmomentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmomentBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saransh/gmoments/GmomentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saransh.gmoments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmomentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
